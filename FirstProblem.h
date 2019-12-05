34e7517c3a930e649ddf086bcd348cfc4c191143
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-05 20:59:48 +0100

    Processing of Question 3

--- FirstProblem.h
+++ FirstProblem.h
@@ -41,9 +41,22 @@ int belongIntoSetTable(const unsigned int T[], int length, int value) {
     return 0;
 }
 
-
-
-
+/***************/
+/* Question 3*/
+void do_sortingSetTable(void (*op) (unsigned int [], int), unsigned int T[], int length) {
+    op(T, length);
+}
+void sortingSetTable(unsigned int T[], int n) {
+    for (int i = 0; i < n-1; ++i) {
+        for (int j = 0; j < n-1-i; ++j) {
+            if (T[j] > T[j+1]) {
+                unsigned int temp = T[j];
+                T[j] = T[j+1];
+                T[j+1] = temp;
+            }
+        }
+    }
+}
 
 
 
--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 #include "FirstProblem.h"
 
 int main() {
-    const unsigned int T[] = {2, 9, 4, 8, 5};
-    int n = do_belongIntoSetTable(belongIntoSetTable, T, (sizeof(T)/ sizeof(T[0])), 98);
-    printf("check if value is in or not = %d\n", n);
+    unsigned int T[] = {2, 9, 4, 8, 5, 22, 7, 1};
+    do_sortingSetTable(sortingSetTable, T, (int) (sizeof(T)/ sizeof(T[0])));
+    printf("Sorted Array = ");
+    for (int i = 0; i < (int) (sizeof(T)/ sizeof(T[0])); ++i) {
+        printf("%d ", *(T+i));
+    }
     return 0;
 }
