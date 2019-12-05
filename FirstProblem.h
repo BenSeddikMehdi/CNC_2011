34f7b261c5a4d521fe6aa475bfd87fc140654394
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-05 20:13:54 +0100

    End of Question 1

--- FirstProblem.h
+++ FirstProblem.h
@@ -16,12 +16,17 @@
 
 /**************************************/
 /* Question 1: Set Table Verification */
-typedef int (*set_table_verify) (unsigned int [], int);
-int do_setTableVerify(set_table_verify op, unsigned int T[], int length) {
+typedef int (*set_table_verify) (const unsigned int [], int);
+int do_setTableVerify(set_table_verify op, const unsigned int T[], int length) {
     return op(T, length);
 }
-int setTableVerify(unsigned int T[], int n) {
-
+int setTableVerify(const unsigned int T[], int n) {//2, 3, 5, 3, 5
+    for (int i = 0; i < n-1; ++i) {
+        for (int j = i; j < n-1; ++j) {
+            if (T[i]==T[j+1]) return 0;
+        }
+    }
+    return 1;
 }
 
 
--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include "FirstProblem.h"
 
 int main() {
-
+    const unsigned int T[] = {2, 9, 4, 8, 5};
+    int n = do_setTableVerify(setTableVerify, T, (sizeof(T)/ sizeof(T[0])));
+    printf("n = %d\n", n);
     return 0;
 }
