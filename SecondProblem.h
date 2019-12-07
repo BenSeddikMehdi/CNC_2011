aea7b3238440854da964917539616dbbcc066d71
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-07 01:30:46 +0100

    Processing Question 1 of the second problem

--- SecondProblem.h
+++ SecondProblem.h
@@ -5,10 +5,15 @@
 #ifndef CNC_2011_SECONDPROBLEM_H
 #define CNC_2011_SECONDPROBLEM_H
 
-/***********************************/
-/* Operations on Mathematical Sets */
-/***********************************/
+/********************/
+/* Hamming Distance */
+/********************/
 
+/**************/
+/* Question 1 */
+int do_hamming_distance(int (*op) (char [], char []), char S1[], char S2[]) {
+    return op(S1, S2);
+}
 
 
 
--- main.c
+++ main.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
 #include "FirstProblem.h"
+#include "SecondProblem.h"
 
 int main() {
-    setList_t* test = NULL, *p = NULL;
-    int T[] = {3, 4, 8, 9}, n = (int) (sizeof(T)/ sizeof(T[0]));
-    test = fillLinkedList(test, T, n);
-    test = do_inserting_value(insertingValue, test, p, 2);
-    test = do_inserting_value(insertingValue, test, p, 22);
-    test = do_inserting_value(insertingValue, test, p, 1);
-    test = do_inserting_value(insertingValue, test, p, 19);
-    printLinkedList(test);
-    free(test);
-    free(p);
+
     return 0;
 }
