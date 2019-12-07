6c1b1c7123dd82b682ea0e2e483544d8333f0510
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-07 02:05:45 +0100

    End of Question 2

--- main.c
+++ main.c
@@ -3,8 +3,6 @@
 #include "SecondProblem.h"
 
 int main() {
-    char S[Nb][L] = {"hola", "ahol", "halo", "hola"};
-    printf("The minimum Hamming distance of the Language is = %d\n",
-            do_language_hamming_distance(language_hamming_distance, S));
+
     return 0;
 }
