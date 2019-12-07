b9c72adffb1062ebaa105d664c0bbffc1fa8c130
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-07 02:19:29 +0100

    End of Question 3-a

--- main.c
+++ main.c
@@ -3,10 +3,6 @@
 #include "SecondProblem.h"
 
 int main() {
-    char bin[9];
-    do_hamming_distance_numbers(hamming_distance_numbers, bin, 128);
-    for (int i = 0; bin[i] != '\0'; ++i) {
-        printf("%d ", bin[i]-'0');
-    }
+
     return 0;
 }
